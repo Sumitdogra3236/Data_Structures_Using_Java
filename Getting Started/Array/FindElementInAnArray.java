2d2edffcebd475daff5130f7bc24c5beb129868f
SUMIT <sumitdogra3236@gmail.com>
2021-05-07 23:23:37 +0530

    Find Element in an Array

---
+++ Getting Started/Array/FindElementInAnArray.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class FindElementInAnArray {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int x = scn.nextInt();
+        int res = find(arr, x);
+        System.out.println(res);
+    }
+
+    public static int find(int arr[], int x) {
+
+        for (int idx = 0; idx < arr.length; idx++) {
+            if (arr[idx] == x) {
+                return idx;
+            }
+        }
+        return -1;
+    }
+}
