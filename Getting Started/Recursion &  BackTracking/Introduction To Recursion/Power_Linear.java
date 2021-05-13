6fa1869ed318305c777e2919a7dbe67929ed881b
SUMIT <sumitdogra3236@gmail.com>
2021-05-13 20:49:34 +0530

    Power Linear

---
+++ Getting Started/Recursion &  BackTracking/Introduction To Recursion/Power_Linear.java
@@ -0,0 +1,22 @@
+// Power Linear using recursion
+import java.io.*;
+import java.util.*;
+
+public class Power_Linear {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+        int res = powerLinear(x, n);
+        System.out.println(res);
+    }
+
+    public static int powerLinear(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return x * powerLinear(x, n - 1);
+    }
+
+}
