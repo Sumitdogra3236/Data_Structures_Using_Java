0736a9381fd48f0a76d4bd920099401682bbd195
SUMIT <sumitdogra3236@gmail.com>
2021-05-13 22:01:26 +0530

    Power Logarithmic

---
+++ Getting Started/Recursion &  BackTracking/Introduction To Recursion/PowerLogarithmic.java
@@ -0,0 +1,27 @@
+// Optimized code to find power Linear.
+import java.io.*;
+import java.util.*;
+
+public class PowerLogarithmic {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+        int res = power(x, n);
+        System.out.println(res);
+    }
+
+    public static int power(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int xPowNb2 = power(x, n / 2);
+        int xPowN = xPowNb2 * xPowNb2;
+        if (n % 2 != 0) {
+            return x * xPowN;
+        }
+        return xPowN;
+    }
+
+}
