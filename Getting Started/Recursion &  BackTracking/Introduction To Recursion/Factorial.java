513f144fd3c9a20f9b2719d431436022360395cc
SUMIT <sumitdogra3236@gmail.com>
2021-05-13 20:20:27 +0530

    Factorial of a number

---
+++ Getting Started/Recursion &  BackTracking/Introduction To Recursion/Factorial.java
@@ -0,0 +1,22 @@
+// Factorial of a number using recursion
+import java.io.*;
+import java.util.*;
+
+public class Factorial {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int res = factorial(n);
+        System.out.println(res);
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+}
