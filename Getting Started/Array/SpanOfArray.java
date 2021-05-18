df0186c62a5857636033ba69f3fa5e009373a1bf
SUMIT <sumitdogra3236@gmail.com>
2021-05-18 10:05:10 +0530

    Span of Array

--- Getting Started/Array/SpanOfArray.java
+++ Getting Started/Array/SpanOfArray.java
@@ -1,36 +1,33 @@
  // Span of Array ( Span = Max Element - Min Element in Array)
     import java.io.*;
-    import java.util.*;
+ import java.util.*;
 
-    public class SpanOfArray {
+ public class SpanOfArray {
 
-        public static void main(String[] args) throws Exception {
-            Scanner scn = new Scanner(System.in);
-            int n = scn.nextInt();
-            int[] arr = new int[n];
+     public static void main(String[] args) throws Exception {
+         Scanner scn = new Scanner(System.in);
+         int n = scn.nextInt();
+         int arr[] = new int[n];
+         for (int i = 0; i < n; i++) {
+             arr[i] = scn.nextInt();
+         }
 
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = scn.nextInt();
-            }
-            int span = SpanOfArray(arr);
-            System.out.println(span);
+         int span = span(arr);
+         System.out.println(span);
 
-        }
+     }
 
-        public static int SpanOfArray(int arr[]) {
-
-            int min = Integer.MAX_VALUE;
-            int max = Integer.MIN_VALUE;
-
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] > max) {
-                    max = arr[i];
-                }
-                if (arr[i] < min) {
-                    min = arr[i];
-                }
-            }
-            return max - min;
-
-        }
-    }
+     public static int span(int arr[]) {
+         int min = Integer.MAX_VALUE;
+         int max = Integer.MIN_VALUE;
+         for (int val : arr) {
+             if (val > max) {
+                 max = val;
+             }
+             if (val < min) {
+                 min = val;
+             }
+         }
+         return max - min;
+     }
+ }
