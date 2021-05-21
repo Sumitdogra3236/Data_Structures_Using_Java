009d0f12c51182a4e5fb066b60b178cc8733fdaa
SUMIT <sumitdogra3236@gmail.com>
2021-05-21 22:17:18 +0530

    Binary Search

--- Getting Started/Array/Binary_Seach.java
+++ Getting Started/Array/Binary_Seach.java
@@ -32,3 +32,37 @@ public class Binary_Seach {
     }
 
 }
+
+// ------------------------------------------------------//
+
+// import java.util.*;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//         Scanner scn = new Scanner(System.in);
+//         int arr[] = { 10, 25, 30, 46, 56, 63, 79, 84, 98, 105, 121 };
+//         int element = 25;
+
+//         System.out.println(BinarySearch(arr, element));
+
+//     }
+
+//     public static int BinarySearch(int[] arr, int element) {
+//         int low = 0;
+//         int high = arr.length - 1;
+
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
+//             if (arr[mid] == element) {
+//                 return mid;
+//             } else if (arr[mid] > element) {
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+
+//         }
+//         return -1; // element not present in array
+//     }
+// }
