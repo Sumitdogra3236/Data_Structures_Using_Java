c997b40405366215190fb7f2ab76d20627d6edf7
SUMIT <sumitdogra3236@gmail.com>
2021-05-25 21:27:36 +0530

    String Compression

---
+++ Getting Started/String, String-Builder & ArrayList/String_Compression.java
@@ -0,0 +1,57 @@
+// String Compression 
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static String compression1(String str) {
+        String res = str.charAt(0) + "";
+        for (int i = 1; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            char prev = str.charAt(i - 1);
+
+            if (curr == prev) {
+                // do nothing
+            } else {
+                res += curr;
+            }
+        }
+
+        return res;
+    }
+
+    public static String compression2(String str) {
+        String res = str.charAt(0) + "";
+
+        int count = 1;
+
+        for (int i = 1; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            char prev = str.charAt(i - 1);
+
+            if (curr == prev) {
+                count++;
+            } else {
+                if (count > 1) {
+                    res += count;
+                    count = 1;
+                }
+                res += curr;
+            }
+        }
+        if (count > 1) {
+            res += count;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        System.out.println(compression1(str));
+        System.out.println(compression2(str));
+    }
+
+}
