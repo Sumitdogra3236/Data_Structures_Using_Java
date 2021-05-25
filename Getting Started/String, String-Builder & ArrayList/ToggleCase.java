4902102b1d8700494a5c4d26405c9eac1d46ec8f
SUMIT <sumitdogra3236@gmail.com>
2021-05-25 22:32:26 +0530

    Toggle

--- Getting Started/String, String-Builder & ArrayList/ToggleCase.java
+++ Getting Started/String, String-Builder & ArrayList/ToggleCase.java
@@ -1,13 +1,21 @@
 
 //        Character to ASCII or UNICODE
+//        Charcter -> UNICODE
 // char ch = 'K';
 // int code = ch;
 // System.out.println(ch);
 
 //        UNICODE or ASCII to character
+//        UNICODE -> Character
 // int code = 109;
 // char ch = (char)code;    -> typecast bcs int-> 4 bytes & char-> 2 ytes
 // System.out.println(ch);
+
+
+// Lower Case -> Upper Case ==> [ UC = 'A' + (ch -'a'); ]
+// Upper Case -> Lower Case ==> [ LC = 'a' + (ch -'A'); ]
+
+
 
 public class ToggleCase {
     
