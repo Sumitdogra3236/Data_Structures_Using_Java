cbad5775e23a0d88a1a09b68e67a18a5f4039d2e
SUMIT <sumitdogra3236@gmail.com>
2021-05-25 22:49:59 +0530

    Toggle Case

--- Getting Started/String, String-Builder & ArrayList/ToggleCase.java
+++ Getting Started/String, String-Builder & ArrayList/ToggleCase.java
@@ -1,4 +1,3 @@
-
 //        Character to ASCII or UNICODE
 //        Charcter -> UNICODE
 // char ch = 'K';
@@ -17,6 +16,34 @@
 
 
 
+import java.io.*;
+import java.util.*;
+
 public class ToggleCase {
-    
+
+    public static String toggleCase(String str) {
+        String res = "";
+        for (int idx = 0; idx < str.length(); idx++) {
+            char ch = str.charAt(idx);
+
+            if (ch >= 'a' && ch <= 'z') { // character is lower case
+                // convert it to upper case
+                char up = (char) ('A' + (ch - 'a'));
+                res += up;
+            } else if (ch >= 'A' && ch <= 'Z') { // character is Upper Case
+                // convert it to lower case
+                char lc = (char) ('a' + (ch - 'A'));
+                res += lc;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        System.out.println(toggleCase(str));
+    }
+
 }
