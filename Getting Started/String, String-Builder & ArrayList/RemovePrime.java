971a9c9fec5c744aed74b602fb1a407db0eb4c16
SUMIT <sumitdogra3236@gmail.com>
2021-05-26 15:00:03 +0530

    Remove Primes

---
+++ Getting Started/String, String-Builder & ArrayList/RemovePrime.java
@@ -0,0 +1,45 @@
+// Remove Prime from Array List
+
+import java.io.*;
+import java.util.*;
+
+public class RemovePrime {
+
+    public static void solution(ArrayList<Integer> al) {
+
+        // for(int i = 0 ;i <= al.size(); i++){ // will not work
+        // int val = al.get(i);
+        // if(isPrime(val)){
+        // al.remove(i);
+        // }
+        // }
+        for (int i = al.size() - 1; i >= 0; i--) {
+            int val = al.get(i);
+            if (isPrime(val)) {
+                al.remove(i);
+            }
+        }
+
+    }
+
+    public static boolean isPrime(int n) {
+        for (int div = 2; div * div <= n; div++) {
+            if (n % div == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        ArrayList<Integer> al = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            al.add(scn.nextInt());
+        }
+        solution(al);
+        System.out.println(al);
+    }
+
+}
