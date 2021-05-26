cfb2d240cefa491e850a221ebafd25fa4c64c803
SUMIT <sumitdogra3236@gmail.com>
2021-05-26 13:54:29 +0530

    ArrayList

---
+++ Getting Started/String, String-Builder & ArrayList/ArrayList_Intro.java
@@ -0,0 +1,10 @@
+// add(val) --> add element at the ends
+// remove(idx) --> remove element accroding to index
+// get(idx) --> gets value of idx
+// set(idx, valie) --> update value at the idx of list
+
+public class ArrayList_Intro {
+
+
+    
+}
