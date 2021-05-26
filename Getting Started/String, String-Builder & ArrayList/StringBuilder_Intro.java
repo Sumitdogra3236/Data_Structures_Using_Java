8b82410a6ef0d2f5d42bb06b029933bf37217fb2
SUMIT <sumitdogra3236@gmail.com>
2021-05-26 22:35:48 +0530

    String Builder

--- Getting Started/String, String-Builder & ArrayList/StringBuilder_Intro.java
+++ Getting Started/String, String-Builder & ArrayList/StringBuilder_Intro.java
@@ -7,6 +7,24 @@ public class StringBuilder_Intro {
         char ch = sb.charAt(2);
         System.out.println(ch);
 
-        System.out.println(sb.reverse()); // -> not change the original array
+        // System.out.println(sb.reverse()); // -> not change the original array
+        System.out.println(sb);
+
+        sb.append(" world"); // --> append single word as '' & string as "" at the end of the sb
+        sb.append(123);
+
+        System.out.println(sb);
+
+        System.out.println(sb.length());
+        
+        sb.setCharAt(1, 'I');
+        System.out.println(sb);
+
+        sb.deleteCharAt(1);
+        System.out.println(sb);
+
+        System.out.println(sb.substring(5));
+
+        sb.toString(); // toString() -> start involving intern pool
     }
 }
