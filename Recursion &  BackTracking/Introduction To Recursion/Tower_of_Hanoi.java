1041b6ac26f16315e0ab4e9c4bf4ceff53ef8b66
SUMIT <sumitdogra3236@gmail.com>
2021-05-27 21:39:28 +0530

    Tower of Hanoi

---
+++ Recursion &  BackTracking/Introduction To Recursion/Tower_of_Hanoi.java
@@ -0,0 +1,31 @@
+// Expectation -> [TOH(10,11,12,3)] -> To print all the valid steps to move all disc from source to destination i.e 10 -> 12
+// Faith 1) -> [TOH(10,12,11,2)] -> Program is capable of printing valid steps to move (n-1) or n == 2 disc from source to Helper i.e 10 -> 12
+// Faith 2) -> [TOH(12,11,10,2)] -> Program is capable of printing valid steps to move (n-1) disc from source to Helper i.e 12 -> 11
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Tower_of_Hanoi {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int src = scn.nextInt();
+        int dest = scn.nextInt();
+        int helper = scn.nextInt();
+
+        toh(src, dest, helper, n);
+    }
+
+    public static void toh(int src, int dest, int helper, int n) {
+        if (n == 0) {
+            return;
+        }
+        toh(src, helper, dest, n - 1);
+        System.out.println(n + "[" + src + " -> " + dest + "]");
+        toh(helper, dest, src, n - 1);
+    }
+
+}
