aaae1a51e48d905c0cb5cfe3859fb75de61355d7
SUMIT <sumitdogra3236@gmail.com>
2021-07-04 23:33:00 +0530

    Sliding Window Maximum

---
+++ Stacks & Queues/Sliding_Window_Maximum.java
@@ -0,0 +1,46 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int k = Integer.parseInt(br.readLine());
+
+        Stack<Integer> st = new Stack<>();
+        int nger[] = new int[arr.length];
+        nger[arr.length - 1] = arr.length;
+        st.push(arr.length - 1);
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            while (st.size() > 0 && arr[i] > arr[st.peek()]) {
+                st.pop();
+            }
+            if (st.size() == 0) {
+                nger[i] = arr.length;
+            } else {
+                nger[i] = st.peek();
+            }
+            st.push(i);
+        }
+
+        for (int i = 0; i <= arr.length - k; i++) {
+            // if(j < i){
+            // j = i;
+            // }
+            int j = i;
+            while (nger[j] < i + k) {
+                j = nger[j];
+            }
+            System.out.println(arr[j]);
+        }
+    }
+}
