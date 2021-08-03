53445c3a2754bc927bd37c697f9ec69270dd7e91
SUMIT <sumitdogra3236@gmail.com>
2021-08-03 22:27:16 +0530

    All Indices of Array

--- Recursion &  BackTracking/Recursion with Arrays/All_Indices_of_Array.java
+++ Recursion &  BackTracking/Recursion with Arrays/All_Indices_of_Array.java
@@ -1,4 +1,24 @@
-// All Indices of Array
+            // All Indices of Array
+// primitive variable cannot be kept uninitialized, it give error variable might not have initialized.
+// reference variable can be kept uninitialized, beacuse java provide the default value of null to reference variable.
+
+                // Logic
+
+// public static int[] allIndices(int[] arr, int x, int idx, int fsf) {
+//         if (idx == arr.length) {
+//             return new int[fsf];
+//         }
+//         int res[];
+//         if (arr[idx] == x) {
+//             res = allIndices(arr, x, idx + 1, fsf + 1);
+//             res[fsf] = idx;
+//         } else {
+//             res = allIndices(arr, x, idx + 1, fsf);
+//         }
+//         return res;
+//     }
+
+
 
 import java.io.*;
 import java.util.*;
@@ -16,28 +36,26 @@ public class All_Indices_of_Array {
         int x = Integer.parseInt(br.readLine());
         int[] iarr = allIndices(arr, x, 0, 0);
 
-        if(iarr.length == 0){
+        if (iarr.length == 0) {
             System.out.println();
             return;
         }
 
-        for(int i = 0; i < iarr.length; i++){
+        for (int i = 0; i < iarr.length; i++) {
             System.out.println(iarr[i]);
         }
     }
 
     public static int[] allIndices(int[] arr, int x, int idx, int fsf) {
-        if(idx == arr.length){
+        if (idx == arr.length) {
             return new int[fsf];
         }
-        
-        int res[] = null;
-        if(arr[idx] == x){
-            res = allIndices(arr,x,idx+1,fsf+1);
+        int res[];
+        if (arr[idx] == x) {
+            res = allIndices(arr, x, idx + 1, fsf + 1);
             res[fsf] = idx;
-        }
-        else {
-            res = allIndices(arr,x,idx+1,fsf);
+        } else {
+            res = allIndices(arr, x, idx + 1, fsf);
         }
         return res;
     }
